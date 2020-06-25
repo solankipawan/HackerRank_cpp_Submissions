95ee8cb92ee8bd8a1d34abb425946e1102826e1e
Pawan Solanki <48286280+P-solanki29@users.noreply.github.com>
2020-06-25 17:50:14 +0530

    Update and rename Hello, World! With C++.txt to Hello, World! With C++.cpp

--- Hello, World! With C++.cpp
+++ Hello, World! With C++.cpp
@@ -1,10 +1,11 @@
-This is a simple challenge to help you practice printing to stdout.
+/*This is a simple challenge to help you practice printing to stdout.
  You may also want to complete Solve Me First in C++ before attempting this challenge.
 
 We're starting out by printing the most famous computing phrase of all time! In the editor below, 
 use either printf or cout to print the string  "Hello, word!" to stdout.
+*/
 
-/*
+
 #include <iostream>
 #include <cstdio>
 using namespace std;
@@ -14,4 +15,3 @@ int main() {
     return 0;
 }
 
-*\
