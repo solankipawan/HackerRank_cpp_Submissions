32eef700ec814fa3d50cd1947e64ab360392644f
P_Solanki29 <pksaug2529@gmail.com>
2020-06-27 15:28:48 +0530

    Solution of vectorerase.cpp

---
+++ vectorerase.cpp
@@ -0,0 +1,40 @@
+/*
+Input Format
+
+The first line of the input contains an integer N .
+The next line contains N space separated integers(1-based index).
+The third line contains a single integer x,denoting the position of an element that should 
+be removed from the vector.
+The fourth line contains two integers a and b denoting the range that should be erased from the vector 
+inclusive of a and exclusive of b.
+*/
+
+#include<iostream>
+#include <bits/stdc++.h>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main(){
+    vector<int> vi;
+    int t;
+    cin >> t;
+    while (t>0)
+    {
+        int input;
+        cin >> input;
+        vi.push_back(input);
+        t--;
+    }
+    int p,q,r;
+    cin >> p >> q >>r;
+    vi.erase(vi.begin()+(p-1)); // 
+    vi.erase(vi.begin()+(q-1), vi.begin()+(r-1));
+    // sort(vi.begin(), vi.end());
+    cout << vi.size() <<endl;
+    for(auto i = vi.begin(); i != vi.end(); i++){
+        cout << *i << " ";
+    }
+return 0;
+}
